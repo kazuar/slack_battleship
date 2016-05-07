1906253a6891ef6bf4c5ab158b6579feacc5e4cf
Tzahi Vidas <tzahi.vidas@gmail.com>
2016-05-06 20:03:39 -0400

    First commit

---
+++ battleship_game.py
@@ -0,0 +1,129 @@
+
+import random
+import numpy as np
+
+BOARD_SIZE = 5
+
+SEA = 0
+SHIP = 1
+HIT = -1
+NO_HIT = -2
+
+SHAPES = [
+    np.ones((1,1)),
+    np.ones((2,1)),
+    np.ones((1,2)),
+    np.ones((1,3)),
+    np.ones((1,4)),
+]
+
+class Player(object):
+    def __init__(self, player_name, size = BOARD_SIZE):
+        self._player_name = player_name
+        self._board = np.zeros((BOARD_SIZE, BOARD_SIZE))
+
+        for shape in SHAPES:
+            shape_added = False
+            while not shape_added:
+                rand_col = random.randint(0, BOARD_SIZE - 1)
+                rand_row = random.randint(0, BOARD_SIZE - 1)
+                shape_added = self._add_shape(shape, (rand_col, rand_row))
+
+    def _add_shape(self, shape, xy_coord):
+        try:
+            size_x, size_y = np.shape(shape)
+            coor_x, coor_y = xy_coord
+            end_x, end_y   = (coor_x + size_x), (coor_y + size_y)
+            if int(self._board[coor_x:end_x, coor_y:end_y].sum()) > 0:
+                return False
+            self._board[coor_x:end_x, coor_y:end_y] = self._board[coor_x:end_x, coor_y:end_y] + shape
+            return True
+        except Exception:
+            return False
+
+    def player_name(self):
+        return self._player_name
+
+    def show(self, for_oponen):
+        for row in self._board:
+            print " ".join([str(int(cell)) for cell in row])
+
+    def guess(self, x, y):
+        val = int(self._board[x][y])
+        if val == 0:
+            self._board[x][y] = NO_HIT
+        elif val == 1:
+            self._board[x][y] = HIT
+        return val
+
+    def get_board_rows(self, hide_ships = False):
+        rows = []
+        for row in self._board:
+            values = []
+            for cell in row:
+                value = int(cell)
+                if hide_ships and value == SHIP:
+                    value = SEA
+                values.append(str(value))
+            rows.append(" ".join(values))
+        return rows
+
+class BattleshipGame(object):
+
+    # def __init__(self):
+    #     return
+
+    def _get_first_player(self):
+        players = [self._player1, self._player2]
+
+        print "getting first player"
+
+        return random.choice(players)
+
+    def _get_current_players(self):
+        current_player = self._current_player
+        opponent = self._player2 if self._player1.player_name() == self._current_player.player_name() else self._player1
+        return self._current_player, opponent
+
+    def start_game(self, player1, player2):
+        print "starting game"
+        self._player1 = Player(player1)
+        self._player2 = Player(player2)
+
+        self._current_player = self._get_first_player()
+
+        print self._current_player
+
+    def get_boards(self):
+        current_player, opponent = self._get_current_players()
+
+        board1_rows = current_player.get_board_rows()
+        board2_rows = opponent.get_board_rows(hide_ships = True)
+
+        return board1_rows, board2_rows
+
+    def turn(self, x, y):
+        # self.print_boards()
+
+        print "turn"
+        print "current player", self._current_player.player_name()
+
+        current_player, opponent = self._get_current_players()
+
+        print "opponent", opponent.player_name()
+
+        result = current_player.guess(x, y)
+
+        self._current_player = opponent
+
+        return result
+
+# def main():
+#     battle_ship_game = BattleshipGame()
+
+#     battle_ship_game.start_game("x", "y")
+
+#     battle_ship_game.print_boards()
+
+# if __name__ == '__main__':
+#     main()
---
+++ slack_bot.py
@@ -0,0 +1,79 @@
+
+import time
+from slackclient import SlackClient
+
+import battleship_game
+
+BOT_TOKEN = "<BOT_API_KEY>"
+CHANNEL_NAME = "general"
+
+def cell_to_str(cell):
+    cell = int(cell)
+    if cell == 0:
+        return ":ocean:"
+    elif cell == 1:
+        return ":boat:"
+    elif cell == -1:
+        return ":fire:"
+    elif cell == -2:
+        return ":x:"
+
+def format_row(row):
+    return row.replace("-2", ":x:").replace("-1", ":fire:").replace("0", ":ocean:").replace("1", ":boat:")
+
+def print_boards(game):
+    board1, board2 = game.get_boards()
+    message = ["player1 \t\t\t\t\t\t\t player2\n"]
+    for i in range(0, len(board1)):
+        message.append("{0} \t\t {1}".format(format_row(board1[i]), format_row(board2[i])))
+    return "\n".join(message)
+
+def print_board(game):
+    message = []
+    for row in game._board:
+        message.append(" ".join([cell_to_str(cell) for cell in row]))
+    return "\n".join(message)
+
+def main():
+    # Create the slackclient instance
+    sc = SlackClient(BOT_TOKEN)
+
+    game = battleship_game.BattleshipGame()
+
+    game.start_game("x", "y")
+
+    # Connect to slack
+    if sc.rtm_connect():
+        # Send first message
+        # sc.rtm_send_message(CHANNEL_NAME, "I'm ALIVE!!!")
+        message = print_boards(game)
+        # message = print_board(game)
+        print message
+        sc.rtm_send_message(CHANNEL_NAME, message)
+
+        while True:
+            # Read latest messages
+            for slack_message in sc.rtm_read():
+                message = slack_message.get("text")
+                user = slack_message.get("user")
+                if not message or not user:
+                    continue
+                if user == "U0XHABXD1":
+                    continue
+                print "message", user, message
+                coords = [int(x) for x in message.replace(",", " ").split(" ")]
+                result = game.turn(coords[0], coords[1])
+                sc.rtm_send_message(CHANNEL_NAME, print_boards(game))
+                if result == battleship_game.HIT:
+                    sc.rtm_send_message(CHANNEL_NAME, "GREAT HIT!")
+                
+
+                # sc.rtm_send_message(CHANNEL_NAME, "<@{}> wrote something...".format(user))
+            # Sleep for half a second
+            time.sleep(0.5)
+    else:
+        print("Couldn't connect to slack")
+
+if __name__ == '__main__':
+    main()
+
